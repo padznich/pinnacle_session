a85b0e0c361e1f2bf2f511cdf389d8e57948005a
PAVEL SLABKO <PAVEL SLABKO>
2017-03-30 22:04:45 +0300

    [ADD] PhantomJS to Selenium

--- session_selenium.py
+++ session_selenium.py
@@ -1,31 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import re
 import time
 
-import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as Ec
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException
 
-chromedriver = "C:\Users\ASUS\Code\pinnacle_session\libs\chromedriver.exe"
+
+# Setup
+customer = "hello"
+passw = "world"
+phantomjs_path = "C:\Users\ASUS\Code\pinnacle_session\driver\phantomjs.exe"
+conf_path = "C:\Users\ASUS\Code\pinnacle_session\conf\conf.ini"
 
 uri_login = "https://www.pinnacle.com/en/login"
 uri_bjmh = "https://www1.pinnacle.com/Casino/MS/BJMH/0"
-customer = "Hello"
-passw = "World"
 
 
-def init_driver(chromedriver_path=None):
+def init_driver(_phantomjs_path=None):
     """
-    At libs-folder ChromeDrives for Linux and Windows
-    :param chromedriver_path:
+    At drivers-folder PhantomJS for Linux and Windows
+    :param _phantomjs_path:
     :return:
     """
-    if not chromedriver_path:
-        chromedriver_path = "/home/pad/code/blackjack-parser/libs/chromedriver"
-    driver = webdriver.Chrome(chromedriver_path)
+    driver = webdriver.PhantomJS(_phantomjs_path)
     driver.wait = WebDriverWait(driver, 10)
     return driver
 
@@ -34,11 +36,11 @@ def login(_driver, _customer, _password):
 
     _driver.get(uri_login)
     try:
-        customer_form = _driver.wait.until(Ec.presence_of_element_located(
+        customer_form = _driver.wait.until(ec.presence_of_element_located(
             (By.NAME, "CustomerId")))
-        password_form = _driver.wait.until(Ec.presence_of_element_located(
+        password_form = _driver.wait.until(ec.presence_of_element_located(
             (By.NAME, "Password")))
-        button = _driver.wait.until(Ec.element_to_be_clickable(
+        button = _driver.wait.until(ec.element_to_be_clickable(
             (By.NAME, "loginSubmit")))
         customer_form.send_keys(_customer)
         password_form.send_keys(_password)
@@ -47,56 +49,51 @@ def login(_driver, _customer, _password):
         print("Login Failed.")
 
 
-def enter_casino(_driver,):
-
-    try:
-        button = _driver.wait.until(Ec.element_to_be_clickable(
-            (By.XPATH, "//a[@href='https://www1.pinnacle.com/en/casino']")))
-        button.click()
-    except TimeoutException:
-        print("Casino entering Failed.")
-
-
-def enter_bjmh(_driver,):
-
-    try:
-        button = _driver.wait.until(Ec.element_to_be_clickable(
-            (By.XPATH, "//a[@data-gamecode='BJMH']")))
-        button.click()
-    except TimeoutException:
-        print("BJMH entering Failed.")
-
-
-def click_coord(_driver, x=None, y=None, delay=None):
-
-    if delay:
-        time.sleep(delay)
-
-    time.sleep(0.2)
-    _driver.maximize_window()
-
-    pyautogui.moveTo(x, y)
-    pyautogui.click()
-
-
-def get_session_uri(d):
+def get_session_uri(_driver):
 
     # Short way
-    login(d, customer, passw)
-    time.sleep(5)
-    d.get(uri_bjmh)
-    click_coord(d, 1155, 365, 30)  # Close frame
-    click_coord(d, 1480, 75, 2)  # Click Options
-    click_coord(d, 1480, 265, 2)  # Click History
+    login(_driver, customer, passw)
+    time.sleep(1)
+    _driver.get(uri_bjmh)
+    time.sleep(1)
 
-    d.switch_to.window(d.window_handles[1])
-    uri_session = d.current_url
+    page_html_unicode = _driver.page_source
+    _driver.quit()
 
-    d.quit()
-    session = re.match(r"(.*GameSession=)(.*)(.{3})", uri_session)
-    return session.groups(2)[1]
+    page_html_str = page_html_unicode.encode('utf-8')
+    page_html_str = "".join([i for i in page_html_str.split("\n")])
+
+    session = re.match(r"(.*)Token=(\w+)&amp(.*)", page_html_str)
+
+    return session.groups()[1]
+
+
+def update_conf_session(session):
+
+    with open(conf_path, "r") as f:
+
+        old_lines_list = []
+        for line in f.readlines():
+            if line[:7] == "session":
+                line = "session={}\n".format(session)
+            old_lines_list.append(line)
+        new_text = "".join(old_lines_list)
+
+    with open(conf_path, "w") as f:
+        f.write(new_text)
+
+
+def update_session():
+    """
+    Function for parser.py
+    :return:
+    """
+    _d = init_driver(phantomjs_path)
+    session = get_session_uri(_d)
+
+    update_conf_session(session)
+
 
 if __name__ == "__main__":
 
-    _d = init_driver(chromedriver)
-    print get_session_uri(_d)
+    update_session()
